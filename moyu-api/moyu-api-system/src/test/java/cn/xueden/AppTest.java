69e0436bd711d2b8fc47a7bd4f2dd91d4d4dac51
goodym <11184629@qq.com>
2020-09-24 13:04:24 +0800

    创建接口微服务

---
+++ moyu-api/moyu-api-system/src/test/java/cn/xueden/AppTest.java
@@ -0,0 +1,20 @@
+package cn.xueden;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
